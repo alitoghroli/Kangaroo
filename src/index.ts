9f102184f88e71d6746213df65aea9088bdd4fd8
alitoghroli <alitoghroliam@gmail.com>
2020-08-26 13:33:45 +0430

    repaire project logic3

--- src/index.ts
+++ src/index.ts
@@ -10,45 +10,18 @@ class Kangaroo {
   }
 }
 
-const EvenOROdd = (input: number): boolean => {
-  return input % 2 === 0;
-};
-
 const CompareKang = (input: number[]): boolean => {
   const kang1: KANG = new Kangaroo(input[0], input[1]);
   const kang2: KANG = new Kangaroo(input[2], input[3]);
-  const t1 = EvenOROdd(kang1.start + kang1.height);
-  const t2 = EvenOROdd(kang2.start + kang2.height);
 
-  let jumpCount2: number;
-  let jumpCount1: number;
-
-  if (t1 === t2) {
-    if (kang1.start > kang2.start) {
-      for (let i = 0; !Number.isInteger(jumpCount2); i++) {
-        jumpCount2 =
-          (kang1.start - kang2.start + kang1.height * i) / kang2.height;
-        if (Number.isInteger(jumpCount2)) {
-          const Samepoint = kang2.start + kang2.height * jumpCount2;
-          console.log('YES and the same point is: ' + Samepoint);
-          return true;
-        }
-      }
-    } else {
-      for (let i = 0; !Number.isInteger(jumpCount1); i++) {
-        jumpCount1 =
-          (kang2.start - kang1.start + kang2.height * i) / kang1.height;
-        if (Number.isInteger(jumpCount1)) {
-          const Samepoint = kang1.start + kang1.height * jumpCount1;
-          console.log('YES and the same point is: ' + Samepoint);
-          return true;
-        }
-      }
-    }
-  } else {
-    console.log('NO');
-    return false;
+  const x: number = (kang1.start - kang2.start) / (kang2.height - kang1.height);
+  if (Number.isInteger(x) && x > 0) {
+    const SamePoint = kang2.height * x + kang2.start;
+    console.log('YES and the same Point is : ' + SamePoint);
+    return true;
   }
+  console.log('NO');
+  return false;
 };
 
-CompareKang([0, 2, 5, 2]);
+CompareKang([0, 7, 2000, 11]);
