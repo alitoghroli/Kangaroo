3291e3dd2f3cfcbdf3d0fde4a800c3551aa26fa7
alitoghroli <alitoghroliam@gmail.com>
2020-08-26 10:07:51 +0430

    repaire project logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 interface KANG {
   start: number;
   height: number;
-  jumpPoints(): number[];
+  jump(): number;
 }
 
 class Kangaroo {
@@ -9,8 +9,8 @@ class Kangaroo {
     this.start = start;
     this.height = height;
   }
-  jumpPoints(): number[] {
-    return [this.start + this.height, this.start - this.height];
+  jump(): number {
+    return this.start + this.height;
   }
 }
 
@@ -18,16 +18,48 @@ const CompareKang = (input: number[]): boolean => {
   const kang1: KANG = new Kangaroo(input[0], input[1]);
   const kang2: KANG = new Kangaroo(input[2], input[3]);
 
-  for (let i = 0; i < kang1.jumpPoints().length; i++) {
-    for (let j = 0; j < kang2.jumpPoints().length; j++) {
-      if (kang1.jumpPoints()[i] === kang2.jumpPoints()[j]) {
-        console.log('YES and same Point is: ' + kang2.jumpPoints()[j]);
-        return true;
-      }
+  const BigStart: number =
+    kang1.start > kang2.start ? kang1.start : kang2.start;
+
+  console.log('Hello to Jump kangaroo !');
+  console.log(
+    '[kangaroo 1] start poin :' +
+      kang1.start +
+      ' and jump height :' +
+      kang1.height
+  );
+  console.log(
+    '[kangaroo 2] start poin :' +
+      kang2.start +
+      ' and jump height :' +
+      kang2.height
+  );
+  console.log("let's start jumping...");
+  console.log('------------------------------------');
+  do {
+    if (kang1.start < kang2.start) {
+      kang1.start = kang1.jump();
+      console.log('[kangaroo 1] jumped...');
+      console.log('[kangaroo 1] start point change to : ' + kang1.start);
+    } else {
+      kang2.start = kang2.jump();
+      console.log('[kangaroo 2] jumped...');
+      console.log('[kangaroo 2] start point change to : ' + kang2.start);
     }
-  }
+
+    if (kang1.start === kang2.start) {
+      console.log('YES and same Point is : ' + kang1.start);
+      return true;
+    } else {
+      console.log('[kangaroo 1] current Point : ' + kang1.start);
+      console.log('[kangaroo 2] current Point : ' + kang2.start);
+      console.log('Not yet! Again...');
+      console.log('-----------------------------------------------------');
+    }
+  } while (kang1.start < Math.pow(BigStart, 2));
+
   console.log('NO!');
   return false;
 };
 
-CompareKang([0, 3, 5, 2]);
+CompareKang([0, 80, 500, 2]);
